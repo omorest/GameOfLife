5892424bf8ceaa626e297b82cfc3186c972f9729
omorest <oscar.moreira.estevez@gmail.com>
2020-03-07 21:50:47 +0000

    main test

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include "include/Tablero.h"
 
 int main() {
+
   int rows, cols;
 
   cout << "Introduzca el tamaño deseado del tablero" << endl;
@@ -11,8 +12,16 @@ int main() {
   cin >> cols;
 
   Tablero tablero(rows, cols);
-  tablero[0]->setEstado(1);
-  tablero.write();
+
+  tablero.position(0,0)->setEstado(1);
+  tablero.position(0,1)->setEstado(1);
+  tablero.position(1,0)->setEstado(1);
+  tablero.position(1,2)->setEstado(1);
+
+  cout << tablero.position(1,2)->actualizarEstado();
+  
+
+
   exit(1);
   return 0;
 }
