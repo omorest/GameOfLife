7e52c1b444ef870446c444bd667ae79d61ddf6d5
omorest <oscar.moreira.estevez@gmail.com>
2020-03-26 13:13:18 +0000

    indent cell3.h

--- include/Cell3.h
+++ include/Cell3.h
@@ -1,3 +1,13 @@
+/**
+ * @file Cell3.h
+ * @author Óscar Moreira Estévez (alu0101209067@ull.edu.es)
+ * @version 2.0
+ * @date 2020-03-26
+ * Subject: Algoritmos y Estructuras de Datos Avanzadas
+ * Practice: 3
+ * Purpose: The game of life
+ */
+
 #ifndef CELL3
 #define CELL3
 
@@ -6,11 +16,11 @@
 using namespace std;
 
 class Cell3 : public Cell  {
-	public:
-		Cell3(int row, int col);
+  public:
+    Cell3(int row, int col);
 
-		int updateStatus(void);
-  	int getState(void) const;
-  	ostream& show(ostream& os) const;
+    int updateStatus(void);
+    int getState(void) const;
+    ostream& show(ostream& os) const;
 };
 #endif
