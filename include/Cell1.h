838c6be9f40be2cf793bbcdd79ca4d274cce935e
omorest <oscar.moreira.estevez@gmail.com>
2020-03-26 13:05:30 +0000

    ident

--- include/Cell1.h
+++ include/Cell1.h
@@ -16,11 +16,11 @@
 using namespace std;
 
 class Cell1 : public Cell {
-	public:
-		Cell1(int row, int col);
+public:
+Cell1(int row, int col);
 
-		int updateStatus(void);
-  	int getState(void) const;
-  	std::ostream& show(std::ostream& os) const;
+int updateStatus(void);
+int getState(void) const;
+std::ostream& show(std::ostream& os) const;
 };
 #endif
