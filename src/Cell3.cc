1edb8e8b34fc673e1e728868aa11ac20fed6c1be
omorest <oscar.moreira.estevez@gmail.com>
2020-03-26 13:14:36 +0000

    Cell3 comments

--- src/Cell3.cc
+++ src/Cell3.cc
@@ -1,10 +1,22 @@
+/**
+ * @file Cell3.h
+ * @author Óscar Moreira Estévez (alu0101209067@ull.edu.es)
+ * @version 2.0
+ * @date 2020-03-26
+ * Subject: Algoritmos y Estructuras de Datos Avanzadas
+ * Practice: 3
+ * Purpose: The game of life
+ */
+
 #include "../include/Cell3.h"
 
+//-------- Constructor ---------
 Cell3::Cell3(int row, int col) : Cell(row, col) 
 {}
 
 
 
+//---- Reglas de seguir viva o morir -----
 int Cell3::updateStatus(void) {
   if (neightborsLive_ == 5 || neightborsLive_ == 1) {
     return 3;
