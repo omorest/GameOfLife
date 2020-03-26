7b6df05ba6cce1cd05f48123bdcc91e3ff726cb9
omorest <oscar.moreira.estevez@gmail.com>
2020-03-26 13:04:41 +0000

    Cell1 comments

--- include/Cell1.h
+++ include/Cell1.h
@@ -1,3 +1,13 @@
+/**
+ * @file Cell1.h
+ * @author Óscar Moreira Estévez (alu0101209067@ull.edu.es)
+ * @version 2.0
+ * @date 2020-03-26
+ * Subject: Algoritmos y Estructuras de Datos Avanzadas
+ * Practice: 3
+ * Purpose: The game of life
+ */
+
 #ifndef CELL1
 #define CELL1
 
--- src/Cell1.cc
+++ src/Cell1.cc
@@ -1,10 +1,20 @@
+/**
+ * @file Cell1.h
+ * @author Óscar Moreira Estévez (alu0101209067@ull.edu.es)
+ * @version 2.0
+ * @date 2020-03-26
+ * Subject: Algoritmos y Estructuras de Datos Avanzadas
+ * Practice: 3
+ * Purpose: The game of life
+ */
+
 #include "../include/Cell1.h"
 
-Cell1::Cell1(int row, int col) : Cell(row, col) 
-{}
+//----------- Constructor ----------
+Cell1::Cell1(int row, int col) : Cell(row, col) {}
 
 
-
+//Regla para seguir viva morir
 int Cell1::updateStatus(void) {
   if (neightborsLive_ == 2 || neightborsLive_ == 3) {
     return 1;
